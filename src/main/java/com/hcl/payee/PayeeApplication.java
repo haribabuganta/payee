38651f4a392708522d4631afedbc29c24c200653
Tejaswinik19 <48977408+Tejaswinik19@users.noreply.github.com>
2019-05-05 12:17:39 +0530

    initial setup

---
+++ src/main/java/com/hcl/payee/PayeeApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.payee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayeeApplication.class, args);
+	}
+
+}
