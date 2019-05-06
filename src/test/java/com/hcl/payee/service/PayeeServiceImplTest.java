6c4d8e9fbe774f2d198cf27f937d7d27f52d261f
PKR950725 <prabhanjan950725@gmail.com>
2019-05-06 11:23:00 +0530

    adding test case

---
+++ src/test/java/com/hcl/payee/service/PayeeServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.hcl.payee.service;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.hcl.payee.pojo.DeletePayeeRequest;
+import com.hcl.payee.repository.PayeeRepository;
+
+import junit.framework.Assert;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PayeeServiceImplTest {
+
+	@Mock
+	PayeeRepository payeeRepository;
+
+	@InjectMocks
+	PayeeServiceImpl payeeServiceImpl;
+
+	static String s1 = "Successfully-deleted";
+	static DeletePayeeRequest deletePayeeRequest = null;
+
+	@BeforeClass
+	public static void setUp() {
+		deletePayeeRequest = new DeletePayeeRequest();
+		deletePayeeRequest.setId(1L);
+		deletePayeeRequest.setOtp(123456);
+
+	}
+
+	@Test
+	public void testvalidateOtp() {
+		String actval = payeeServiceImpl.validateOtp(1L, 123456);
+		Assert.assertEquals(s1, actval);
+		System.out.println("test");
+
+	}
+
+}
